3894194b34cf247df781a481a6fa9f2947d9293a
AnastasiaSemenko <81240157+AnastasiaSemenko@users.noreply.github.com>
2021-10-29 17:14:01 +0300

    Update Runner.java

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,3 @@
-import connection.ConnectionPool;
-import connection.ConnectionPoolException;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Runner {
     public static void main(String[] args) {
 
